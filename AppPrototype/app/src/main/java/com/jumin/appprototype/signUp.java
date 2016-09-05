0143a34e94378e1ef09670f3750d2fb03949958b
Internet <eddybob1234@yahoo.com>
2016-09-05 08:58:33 -0700

    App Prototype

--- AppPrototype/app/src/main/java/com/jumin/appprototype/signUp.java
+++ AppPrototype/app/src/main/java/com/jumin/appprototype/signUp.java
@@ -1,8 +1,10 @@
 package com.jumin.appprototype;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.text.Html;
+import android.widget.TextView;
 
 public class signUp extends AppCompatActivity {
 
