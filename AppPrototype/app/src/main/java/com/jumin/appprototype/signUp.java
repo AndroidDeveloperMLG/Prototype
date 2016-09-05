90ecb360695138db06be676a3a4fe223baac7e39
Internet <eddybob1234@yahoo.com>
2016-09-05 11:01:27 -0700

    App Prototype

--- AppPrototype/app/src/main/java/com/jumin/appprototype/signUp.java
+++ AppPrototype/app/src/main/java/com/jumin/appprototype/signUp.java
@@ -1,12 +1,22 @@
 package com.jumin.appprototype;
 
+import android.content.Intent;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
 import android.widget.TextView;
 
 public class signUp extends AppCompatActivity {
+
+    public void login(View view) throws InterruptedException { // Fade out all current elements and fade in sign up elements
+
+        Intent leadToSignUp = new Intent(signUp.this, login.class);
+        signUp.this.startActivity(leadToSignUp);
+        overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
